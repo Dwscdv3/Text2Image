af7fc38d20eca30980083dc333c64b6eb7d415ca
Dwscdv3 <dwscdv3@hotmail.com>
2016-08-25 16:59:33 +0800

    huge of changes

--- main.js
+++ main.js
@@ -11,41 +11,69 @@ function resize() {
 window.onresize = resize;
 resize();
 
-divPeek.onmouseover = function () {
-    if (navigator.userAgent.indexOf("Edge") >= 0) {
-        divCanvasPreview.className = "noblur-transition";
-        setTimeout(function () {
-            divMainContainer.className = "hide-transition";
-        }, 500);
-    } else {
-        divMainContainer.className = "hide-transition";
-        divCanvasPreview.className = "noblur-transition";
+var previewEnabled = true;
+function startPeek() {
+    if (previewEnabled) {
+        if (navigator.userAgent.indexOf("Edge") >= 0) {
+            divCanvasPreview.classList.add("noblur-transition");
+            setTimeout(function () {
+                divMainContainer.classList.add("hide-transition");
+            }, 500);
+        } else {
+            divMainContainer.classList.add("hide-transition");
+            divCanvasPreview.classList.add("noblur-transition");
+        }
     }
 }
-divPeek.onmouseout = function () {
+function stopPeek() {
     if (navigator.userAgent.indexOf("Edge") >= 0) {
-        divMainContainer.className = "";
+        divMainContainer.classList.remove("hide-transition");
         setTimeout(function () {
-            divCanvasPreview.className = "";
+            divCanvasPreview.classList.remove("noblur-transition");
         }, 600);
     } else {
-        divCanvasPreview.className = "";
-        divMainContainer.className = "";
+        divCanvasPreview.classList.remove("noblur-transition");
+        divMainContainer.classList.remove("hide-transition");
     }
 }
+divPeek.onclick = function () {
+    if (previewEnabled) {
+        previewEnabled = false;
+        divPeek.title = "预览已禁用\n点击以启用";
+        divPeek.classList.add("color-disabled");
+        divCanvasPreview.classList.add("hide-transition");
+        stopPeek();
+    } else {
+        previewEnabled = true;
+        divPeek.title = "预览\n点击以禁用";
+        divPeek.classList.remove("color-disabled");
+        divCanvasPreview.classList.remove("hide-transition");
+        startPeek();
+    }
+}
+divPeek.onmouseover = startPeek;
+divPeek.onmouseout = stopPeek;
 
+var listItemCount = 0;
 buttonAdd.onclick = function () {
-    divList.innerHTML += generateListItem();
+    divList.appendChild(generateListItem(++listItemCount));
 };
 
 function rand(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-function generateListItem() {
-    return "";
+function generateListItem(index) {
+    var element = document.createElement("div");
+    element.id = "divListItem" + listItemCount;
+    element.className = "listrow";
+    element.innerHTML = '<div id="" class="listitem listitem-text"><input type="text" name="text' + listItemCount + '" value=""></div><div id="" class="listitem listitem-pos"><div class="listitem div-pos-number"><input id="numberPosX' + listItemCount + '" type="number" name="posx' + listItemCount + '" step="1" value="0"></div><div class="listitem div-pos-number"><input id="numberPosY' + listItemCount + '" type="number" name="posy' + listItemCount + '" step="1" value="0"></div></div><div id="" class="listitem listitem-font"><select id="selectFontFamily' + listItemCount + '" class="select-font-family" title="字体家族"><optgroup label="Windows 中文字体"><option>等线</option><option>微软雅黑</option><option>隶书</option><option>幼圆</option><option>仿宋</option><option>楷体</option><option>黑体</option><option>宋体</option></optgroup><optgroup label="Office 2007 中文字体"><option>方正舒体</option><option>方正姚体</option><option>华文彩云</option><option>华文仿宋</option><option>华文行楷</option><option>华文琥珀</option><option>华文楷体</option><option>华文隶书</option><option>华文宋体</option><option>华文细黑</option><option>华文新魏</option><option>华文中宋</option></optgroup><optgroup label="Windows 拉丁文无衬线字体"><option>Arial</option><option>Calibri</option><option>Segoe UI</option><option>Tahoma</option><option>Verdana</option></optgroup><optgroup label="Windows 拉丁文衬线字体"><option>Times New Roman</option></optgroup><optgroup label="Windows 拉丁文手写字体"><option>Brush Script MT</option><option>Comic Sans MS</option><option>Freestyle Script</option><option>Old English Text MT</option><option>Segoe Print</option><option>Segoe Script</option></optgroup><optgroup label="Windows 拉丁文等宽字体"><option>Consolas</option><option>Courier New</option><option>Lucida Console</option></optgroup><optgroup label="Windows 拉丁文 ITC 系列字体"><option>Blackadder ITC</option><option>Bradley Hand ITC</option><option>Edwardian Script ITC</option><option>Eras ITC</option><option>Juice ITC</option><option>Kristen ITC</option><option>Snap ITC</option><option>Tempus Sans ITC</option><option>Viner Hand ITC</option></optgroup></select><div class="listitem div-font-size"><input id="numberFontSize' + listItemCount + '" type="number" name="size' + listItemCount + '" min="0" value="16" title="字体大小 (px)"></div><div class="div-font-weight"><div id="divFontWeightValue' + listItemCount + '" class="div-font-weight-value">400</div><input id="rangeFontWeight' + listItemCount + '" class="range-font-weight" type="range" min="100" max="900" step="100" value="400" list="datalistFontWeights"oninput="onRangeFontWeightChange(' + listItemCount + ');" title="字体粗细"></div><button id="buttonItalic' + listItemCount + '" type="button" title="斜体" style="width: 52px"onclick="onButtonItalicClick(' + listItemCount + ');"><span class="italic" style="font-size: 30px">i</span></button></div><div id="" class="listitem listitem-color"><input id="color' + listItemCount + '" type="color" name="" value="#000000" title="#000000" onchange="onColorChange(' + listItemCount + ');"></div><div id="" class="listitem listitem-op"><button id="buttonRemove' + listItemCount + '" class="font-icon color-warning" title="移除" onclick="onButtonRemoveClick(' + listItemCount + ');">&#xE107;</button></div>';
+    return element;
 }
 
+function onButtonRemoveClick(index) {
+    document.getElementById("divListItem" + index).remove();
+}
 function onRangeFontWeightChange(index) {
     document.getElementById("divFontWeightValue" + index).innerHTML =
         document.getElementById("rangeFontWeight" + index).value;
